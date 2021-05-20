1c489076e5882beae21105d93aa0e8a2fcfc8584
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-20 22:12:49 +0530

    Add files via upload

---
+++ Customer class/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Class_customer
+{
+    class customer
+    {
+        static void main(string[] args)
+        {
+
+        public int CustID;
+        public string Name;
+        public string Address;
+
+
+        customer()
+        {
+            CustID = 1101;
+            Name = "Tom";
+            Address = "USA";
+        }
+
+
+        public void displayData()
+        {
+            Console.WriteLine("Customer=" + CustID);
+            Console.WriteLine("Name=" + Name);
+            Console.WriteLine("Address=" + Address);
+        }
+    }
+}
+
+
+    
+
+
+   
+    
